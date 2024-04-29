bf172f28d83609b92a35a3111fff2be1f56f7ae5
swapyshelke <swapyshelke02gmail.com>
2024-04-29 15:15:26 +0530

    setup completed

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <>
+      <h1>hello there</h1>
+      <Button>Hello there</Button>
+    </>
+  )
+}
+
+export default App
